d29181ba7d289da36a4db9e1c5ebab023628ad9d
George Kinsman <george@georgekinsman.com>
2015-07-14 14:41:30 +1000

    Initial commit

---
+++ OracleConnectionSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleConnectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["OracleConnectionString"];
+
+            var factory = DbProviderFactories.GetFactory(connectionString.ProviderName);
+
+            Console.WriteLine("{0} Provider Factory successfully resolved", connectionString.ProviderName);
+
+            var connection = factory.CreateConnection();
+
+            connection.ConnectionString = connectionString.ConnectionString;
+
+            connection.Open();
+
+            Console.WriteLine("Connection successfully opened to {0}", connectionString.ConnectionString);
+
+            Console.ReadLine();
+        }
+    }
+}
